5cb5cd69baed89cc1347302c827b553a9ad48ade
grepml <grep.ml@gmail.com>
2016-03-27 02:33:49 +0800

    removed redundancy codes.

--- clojure/abcdefghppp.clj
+++ clojure/abcdefghppp.clj
@@ -1,28 +1,23 @@
 ; http://m2.hkgolden.com/view.aspx?message=6307521&type=SW&page=1
 ; binc
 
-(require 'clojure.set)
-
 (defn make-seq []
-  (map read-string
-       (for [x (range 10)
-             y (range 10)
-             :when (and (not= x y) (not= x 0))]
-         (apply str [x y]))))
+  (for [x (range 10) 
+        y (range 10) 
+        :when (and (not= x y) (not= x 0))]
+    (+ (* 10 x) y)))
 
 (defn test-form []
-  (let [mq (make-seq)
-        v2s (fn [v] (reduce #(clojure.set/union %1 (set (str %2))) #{} v))]
-    (->>
-     (for [ab mq
-           cd mq
-           gh mq
-           :let [ef (- ab cd)
-                 ppp (+ ef gh)]
-           :when (and (= 1 (count (set (str ppp))))
-                      (> ppp 99))]
-       [ab cd ef gh ppp])
-     (filter #(= 9 (count (v2s %)))))))
+  (let [mq (make-seq)]
+    (for [ab mq
+          cd mq
+          gh mq
+          :let [ef (- ab cd) 
+                ppp (+ ef gh) 
+                form [ab cd ef gh ppp]]
+          :when (and (= ppp 111)
+                     (= 9 (->> form (apply str) set count)))]
+      form)))
 
 (prn (test-form))
 

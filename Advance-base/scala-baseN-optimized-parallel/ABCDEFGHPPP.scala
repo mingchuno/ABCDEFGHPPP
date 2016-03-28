20306912d5b8722e57467c17f50f23270877d922
loyuehei <logap@me.com>
2016-03-28 17:28:03 +0800

    Add random solution printing, add back missing test device in readme

--- Advance-base/scala-baseN-optimized-parallel/ABCDEFGHPPP.scala
+++ Advance-base/scala-baseN-optimized-parallel/ABCDEFGHPPP.scala
@@ -1,28 +1,26 @@
+import scala.util.Random
+
 object ABCDEFGHPPP {
+  val base = 22
+  val p = 1
+  val ppp = p * Math.pow(base,2) + p * Math.pow(base,1) + p
 
   def intToChar(x:Int) = {
     if( x > 9) (x - 10 + 'a'.toInt).toChar
     else (x - 1 + '1'.toInt).toChar
   }
 
-  def main(args: Array[String]): Unit = {
+  def possiblePermuatationN(xs: List[Int], n: Int) = {
+    xs.combinations(n)
+      .flatMap(_.permutations)
+      .toStream    // lazy stream
+      .par         // parallel collection
+  }
 
-    val start = System.currentTimeMillis()
-    println("Start Calculation")
-
-    val base = 16
-    val p = 1
-    val ppp = p * Math.pow(base,2) + p * Math.pow(base,1) + p
+  def solveABCDEFGHPPP = {
 
     val possibleTwoDigitFilter: (List[Int]) => Boolean = {
       case List(leading1, _, leading2, _) =>  leading1 > 0 && leading2 > 0
-    }
-
-    def possiblePermuatationN(xs: List[Int], n: Int) = {
-      xs.combinations(4)
-        .flatMap(_.permutations)
-        .toStream    // lazy stream
-        .par         // parallel collection
     }
 
     val possibleToTry = (0 until base)
@@ -37,7 +35,7 @@ object ABCDEFGHPPP {
           case List(e, f, g, h) => ppp - (g * base + h) ==  e * base + f
         }
 
-    val solutions = efghs
+    efghs
       .flatMap {
         case efgh @ List(e, f, g, h) =>
           // store the ef for reuse
@@ -51,18 +49,27 @@ object ABCDEFGHPPP {
             }
             .map(_ ++ efgh)
       }
+  }
 
-    solutions
-      .take(50)
-      .map(xs => xs.map(intToChar))
-      .foreach {
-        case List(a,b,c,d,e,f,g,h) =>
-        // Hide printing log in benchmark
-        println(s"$a$b - $c$d = $e$f, $e$f + $g$h = $p$p$p")
-      }
+  def main(args: Array[String]): Unit = {
+
+    val start = System.currentTimeMillis()
+    println("Start Calculation")
+
+    val solutions = solveABCDEFGHPPP
 
     println(s"Total Time used to solve AB - CD = 111 - GH = EF (Base $base): ${System.currentTimeMillis() - start}ms")
     println(s"Total number of solutions = ${solutions.size}")
 
+    if(solutions.size > 50 ) println(s"Randomly print out 50 solution :")
+    
+    Random.shuffle(solutions.toList)
+      .take(50)
+      .map(xs => xs.map(intToChar))
+      .foreach {
+        case List(a,b,c,d,e,f,g,h) =>
+          // Hide printing log in benchmark
+          println(s"$a$b - $c$d = $e$f, $e$f + $g$h = $p$p$p")
+      }
   }
 }

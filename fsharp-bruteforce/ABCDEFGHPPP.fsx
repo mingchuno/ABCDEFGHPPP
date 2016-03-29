2221bfbfd22abc3f91ca320fb6a5a975580b53af
Rex Ng <rexng@thisisnoble.com>
2016-03-29 09:00:29 +0800

    Minor refactoring

--- fsharp-bruteforce/ABCDEFGHPPP.fsx
+++ fsharp-bruteforce/ABCDEFGHPPP.fsx
@@ -3,11 +3,12 @@ let rec getPermutations list =
     let rec distribute e = function
         | [] -> [[e]]
         | (h :: t) as l -> (e :: l) :: [for l in distribute e t -> h :: l]
+
     match list with
     | [] -> [[]]
     | h :: t -> List.collect (distribute h) (getPermutations t)
 
-let solvePermutation = function
+let canMatchEquation = function
     | [a; b; c; d; e; f; g; h; p; _] when a <> 0 && c <> 0 && e <> 0 && g <> 0 && p <> 0 ->
         let v1 = a * 10 + b
         let v2 = c * 10 + d 
@@ -19,6 +20,8 @@ let solvePermutation = function
 
 [0..9]
 |> getPermutations
-|> List.filter solvePermutation
-|> List.iter (function [a; b; c; d; e; f; g; h; p; _] -> 
-                        printfn "(%i%i - %i%i = %i%i) + %i%i = %i%i%i" a b c d e f g h p p p | _ -> failwith "Unexpected")
+|> List.filter canMatchEquation
+|> List.iter (function 
+                | [a; b; c; d; e; f; g; h; p; _] -> 
+                        printfn "(%i%i - %i%i = %i%i) + %i%i = %i%i%i" a b c d e f g h p p p
+                | _ -> failwith "Unexpected")

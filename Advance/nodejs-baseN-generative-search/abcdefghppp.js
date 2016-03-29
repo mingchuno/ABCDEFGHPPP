b9d4f3a60f62a817660cbb7699f2e961e73aa649
Jack Tang <jackhftang@hotmail.com>
2016-03-29 09:44:33 +0800

    updated readMe and minor tuning

--- Advance/nodejs-baseN-generative-search/abcdefghppp.js
+++ Advance/nodejs-baseN-generative-search/abcdefghppp.js
@@ -1,5 +1,14 @@
-function abcdefghppp(base, width){
+function abcdefghppp(base, width, size){
+  size = typeof size === 'undefined' ? 50 : size;   // max output size
   var i;
+  var ans = [];
+  var cnt = 0;
+  var used1 = new Array(base);
+  var used2 = new Array(base);
+  var sp = new Array(width);
+
+  var diff = [new Array(base), new Array(base)];
+  for(i=0; i<2; i++) for(var j=0; j<base; j++) diff[i][j] = [];
 
   // EXP[i] = base^i
   var EXP = new Array(width);
@@ -10,28 +19,9 @@ function abcdefghppp(base, width){
   var e = 1;
   for(i=0; i<width; i++) e = base*e+1;
 
-  // minimum value of a,b,c,d in form like 2034..
-  var MIN;
-  if( width == 1 ) MIN = 0;
-  else {
-    MIN = 2*base;
-    for(i=2; i<width; i++) MIN = base*MIN + i + 1;
-  }
-
-  var ans = [];
-  var cnt = 0;
-  var used1 = new Array(base);
-  var used2 = new Array(base);
-
-  function print(a,b,c,d){
-    var sa = a.toString(base);
-    var sb = b.toString(base);
-    var sc = c.toString(base);
-    var sd = d.toString(base);
-    var se = (a-b+d).toString(base);
-    return [sa,'-',sb,'=',sc,', ',sc,'+',sd,'=',se].join('');
-  }
-
+  // just to make sure b has at least width digits
+  var MIN=1;
+  for(i=1; i<width; i++) MIN = MIN*base;
 
   // used-tables helpers
   function reset(used){
@@ -76,11 +66,10 @@ function abcdefghppp(base, width){
     var vs = [];
     for(i=0; i<used2.length; i++) if( !used2[i] ) vs.push(i);
 
-    // O(B)
+    // O(B*B)
     // diff[0][i] = all possible pairs of digits [a,b] such that b-a = i
     // diff[1][i] = all possible pairs of digits [a,b] such that 10+b-a= i
-    var diff = [new Array(base), new Array(base)];
-    for(i=0; i<2; i++) for(var j=0; j<base; j++) diff[i][j] = [];
+    for(i=0; i<2; i++) for(var j=0; j<base; j++) diff[i][j].splice(0);
     for(i=0; i<vs.length; i++){
       for(var k=i+1; k<vs.length; k++){
         var v1 = vs[i], v2 = vs[k];
@@ -91,8 +80,8 @@ function abcdefghppp(base, width){
 
     function diffSearch(i, borrow, a, b){
       if( i === width ){
-        if( a > b && b >= MIN ){
-          if(cnt < 50) ans.push(print(a,b,c,e-c));
+        if( a > b && b > MIN ){
+          if(ans.length < size) ans.push([a,b,c,e-c]);
           cnt++;
         }
         return;
@@ -109,7 +98,7 @@ function abcdefghppp(base, width){
         }
       }
     }
-    var sp = new Array(width);
+
     for(i=0, t=c; i<width; i++, t = Math.floor(t/base) ) sp[i] = t%base;
     diffSearch(0, 0, 0, 0);
     // due to symmetry of c and d
@@ -124,15 +113,25 @@ function abcdefghppp(base, width){
   return [cnt, ans];
 }
 
+function print(base,ans){
+  var sa = ans[0].toString(base);
+  var sb = ans[1].toString(base);
+  var sc = ans[2].toString(base);
+  var sd = ans[3].toString(base);
+  var se = (ans[0]-ans[1]+ans[3]).toString(base);
+  return [sa,'-',sb,'=',sc,', ',sc,'+',sd,'=',se].join('');
+}
+
 var s2 = '============================================================';
 [16,22,28,34].forEach(function(base) {
+  var width = 2;
   var s = Date.now();
-  var x = abcdefghppp(base, 2);
+  var x = abcdefghppp(base, width);
   var e = Date.now();
   var cnt = x[0], ans = x[1];
   s2 += `
 Case base=${base}
-${ans.sort().join('\n')}
+${ans.map((x) => print(base,x)).sort().join('\n')}
 time: ${((e - s) / 1000)}s
 number of solutions for base-${base} : ${cnt}
 ============================================================`
@@ -142,17 +141,17 @@ require('fs').writeFileSync('output_w2.txt', s2);
 
 var s4 = '============================================================';
 [17,21,25,29].forEach(function(base) {
+  var width = 4;
   var s = Date.now();
-  var x = abcdefghppp(base, 4);
+  var x = abcdefghppp(base, width);
   var e = Date.now();
   var cnt = x[0], ans = x[1];
   s4 += `
 Case base=${base}
-${ans.sort().join('\n')}
+${ans.map((x) => print(base,x)).sort().join('\n')}
 time: ${((e - s) / 1000)}s
 number of solutions for base-${base} : ${cnt}
-============================================================
-`
+============================================================`
 });
 require('fs').writeFileSync('output_w4.txt', s4);
 

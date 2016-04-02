59ee50901389812f9e06c465a4fdc942e5415d4a
mingchun.or <mingchun.or@harborx.com>
2016-04-02 18:57:56 +0800

    [julia-permutation] use permutations instead of nthperm

--- julia-permutations/abcdefghppp.jl
+++ julia-permutations/abcdefghppp.jl
@@ -4,28 +4,26 @@ simple permutation brute fource
 =#
 array = [0 ,2 ,3, 4, 5, 6, 7 ,8, 9]
 
-# Since 9P9 == 9P8
-for i=1:factorial(9)
-  temp_arr = nthperm(array, i)
-  a = temp_arr[1]
+for arr in permutations(array)
+  a = arr[1]
   if a == 0 continue end # fail fast
 
-  b = temp_arr[2]
+  b = arr[2]
 
-  c = temp_arr[3]
+  c = arr[3]
   if c == 0 continue end # fail fast
 
-  d = temp_arr[4]
+  d = arr[4]
 
-  e = temp_arr[5]
+  e = arr[5]
   if e == 0 continue end # fail fast
 
-  f = temp_arr[6]
+  f = arr[6]
 
-  g = temp_arr[7]
+  g = arr[7]
   if g == 0 continue end # fail fast
 
-  h = temp_arr[8]
+  h = arr[8]
 
   ab = a*10 + b
   cd = c*10 + d

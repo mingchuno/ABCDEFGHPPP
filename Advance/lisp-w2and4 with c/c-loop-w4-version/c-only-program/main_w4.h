98764dafc63da2bbd086eea3e60e7dfe8c56c273
dng8888-mac <dng8888@mac.com>
2016-05-02 16:58:23 +0800

    refactor lisp (after upgrade the c loop version to w4 as ref)
    A faster lisp version after upgrade the c loop version to w4.  The new
    version is much faster (0.14 seconds vs 30 seconds under the same
    computer in base 34).  W4 base 29 is sort of run-able and only 10 times
    slower than the reference c version (upgraded by me).  Still 3,000
    times slower than generative c but at least got a version that can
    finish just over one day (not one week).
    
    Also trying the github desktop version for pull request.  Fork on
    github then clone, after update commit and sync.  Then pull request.
    Still it goes.

---
+++ Advance/lisp-w2and4 with c/c-loop-w4-version/c-only-program/main_w4.h
@@ -0,0 +1,12 @@
+//
+//  main_w4.h
+//  c-only-program
+//
+
+
+#ifndef main_w4_h
+#define main_w4_h
+
+extern int main_w4(int a);
+
+#endif /* main_w4_h */
---
+++ Advance/lisp-w2and4 with c/c-loop-w4-version/c-only-program/main_wx.h
@@ -0,0 +1,12 @@
+//
+//  main_w4.h
+//  c-only-program
+//
+// not finished and not going to
+
+#ifndef main_wx_h
+#define main_wx_h
+
+extern int main_wx(int a, int b);
+
+#endif /* main_w4_h */
